15d6d09d4b3a69b02600369f82d51e7f3fe10045
Admin <admin@Admins-MBP.lan>
2024-04-17 20:36:57 -0500

    aws commit

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
+from credentials import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
 app = Flask('__name__')
-model=pickle.load(open('model.pkl','rb'))
+app.config['AWS_ACCESS_KEY_ID'] = AWS_ACCESS_KEY_ID
+app.config['AWS_SECRET_ACCESS_KEY'] = AWS_SECRET_ACCESS_KEY
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=["POST"])
+@app.route('/predict', methods=["POST"])
 def predict():
-    feature=[int(x) for x in request.form.values()]
-    feature_final=np.array(feature).reshape(-1,1)
-    prediction=model.predict(feature_final)
-    return render_template('index.html',prediction_text='Price of House will be Rs. {}'.format(int(prediction)))
+    feature = [int(x) for x in request.form.values()]
+    feature_final = np.array(feature).reshape(-1, 1)
+    prediction = model.predict(feature_final)
+    return render_template('index.html', prediction_text='Price of House will be Rs. {}'.format(int(prediction)))
 
-if(__name__=='__main__'):
+if __name__ == '__main__':
     app.run(debug=True)
-
--- model.py
+++ model.py
@@ -1,10 +1,25 @@
-import pandas as pd 
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
-df=pd.read_json('Data/House_Price.json')
-x=df['Area(in sq. ft)'].values.reshape(-1,1)
-y=df['Price(in Rs.)'].values.reshape(-1,1)
-lin=LinearRegression()
-lin.fit(x,y)
-pickle.dump(lin,open('model.pkl','wb'))
+import boto3
+import botocore
+from credentials import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+bucket_name = 'your-bucket-name'
+object_key = 'path/to/House_Price.json'
+
+try:
+    response = s3.get_object(Bucket=bucket_name, Key=object_key)
+    data = response['Body'].read().decode('utf-8')
+    df = pd.read_json(data)
+except botocore.exceptions.ClientError as e:
+    print(f"Error retrieving data from S3: {e}")
+    # Handle the error appropriately
+
+x = df['Area(in sq. ft)'].values.reshape(-1, 1)
+y = df['Price(in Rs.)'].values.reshape(-1, 1)
+lin = LinearRegression()
+lin.fit(x, y)
+pickle.dump(lin, open('model.pkl', 'wb'))
